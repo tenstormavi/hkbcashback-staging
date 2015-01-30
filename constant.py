673083b0205395c0856a08a75a9cf3ba8294968f
harshitbahl <harshit.bahl@me.com>
2015-01-29 23:00:25 -0600

    grid version 1.5

--- constant.py
+++ constant.py
@@ -5,26 +5,27 @@ Created on Sat Sep 27 14:02:40 2014
 @author: harshitbahl
 """
 
-HEADER = ['merchant','status','product','NVR','click_time','SR','transaction_time',
-'MID','cash_back_amount','transaction_value','VR','transaction_date','uKey','referrer','PID','merchant_ref',
-'voucher_code','transaction_id','UID']
+HEADER = ['merchant', 'status', 'product', 'NVR', 'click_time', 'SR', 'transaction_time',
+			  'MID', 'cash_back_amount', 'transaction_value', 'VR', 'transaction_date',
+			  'uKey', 'referrer', 'PID', 'merchant_ref', 'voucher_code', 'transaction_id','UID']
 
-USERHEADER = ['transaction_id','product', 'merchant', 'transaction_date', 
-              'transaction_value','cash_back_amount', 'status']
+USERHEADER = ['transaction_id', 'product', 'merchant', 'transaction_date', 
+              'transaction_value', 'cash_back_amount', 'status']
 
-USERHEADER_MAP ={'transaction_id':'Transaction ID',
-                 'product':'Product Name',
-                 'merchant':'Merchant Name',
-                 'transaction_date':'Transaction Date',
-                 'cash_back_amount': 'Cash Back Amount',
+USERHEADER_MAP ={'transaction_id'   :'Transaction ID',
+                 'product'          :'Product Name',
+                 'merchant'         :'Merchant Name',
+                 'transaction_date' :'Transaction Date',
+                 'cash_back_amount' : 'Cash Back Amount',
                  'transaction_value':'Transaction Value',
-                 'status':'Status',
-                 'merchant_ref': 'Merchant Ref',
-                 'email':'Username',
-                 'password':'Password',
-                 'password2':'Password',
-                 'phonenumber':'Phone Number'
+                 'status'           :'Status',
+                 'merchant_ref'     : 'Merchant Ref',
+                 'email'            :'Username',
+                 'password'         :'Password',
+                 'password2'        :'Password',
+                 'phonenumber'      :'Phone Number'
                  }
+                 
 MISSING_HEADER = ['transaction_date', 'merchant_ref', 'merchant', 'product',  
                   'transaction_value', 'cash_back_amount', 'status']
 
@@ -78,4 +79,7 @@ ORDER_MAP = {10859:"http://clk.omgt5.com/?AID=682085&PID=10859&WID=55355",
             13255:"http://clk.omgt5.com/?AID=682085&PID=13255&WID=55355",
             11965:"http://clk.omgt5.com/?AID=682085&PID=11965&WID=55355",
             10036:"http://clk.omgt5.com/?AID=682085&PID=10036&WID=55355",
+            12185:"http://clk.omgt5.com/?AID=682085&PID=12185&WID=55355",
+            12495:"http://clk.omgt5.com/?AID=682085&PID=12495&WID=55355",
+            11482:"http://clk.omgt5.com/?AID=682085&PID=11482&WID=55355",
              }
